87bd2af1300891bf80f590cd3daaf30079a4f2f9
Radoslaw Zan <Radoslaw.Zan@gft.com>
2020-05-15 13:05:23 +0200

    Removed unused handler

--- complete/src/main/java/com/example/messagingstompwebsocket/dto/StockInfo.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/dto/StockInfo.java
@@ -2,39 +2,39 @@ package com.example.messagingstompwebsocket.dto;
 
 public class StockInfo {
 
-	private String symbol;
-	private String price;
-	private String arrow;
+    private String symbol;
+    private String price;
+    private String arrow;
 
 
-	public StockInfo(String symbol, String price, String arrow) {
-		this.symbol = symbol;
-		this.price = price;
-		this.arrow = arrow;
-	}
+    public StockInfo(String symbol, String price, String arrow) {
+        this.symbol = symbol;
+        this.price = price;
+        this.arrow = arrow;
+    }
 
-	public String getSymbol() {
-		return symbol;
-	}
+    public String getSymbol() {
+        return symbol;
+    }
 
-	public void setSymbol(String symbol) {
-		this.symbol = symbol;
-	}
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
 
-	public String getPrice() {
-		return price;
-	}
+    public String getPrice() {
+        return price;
+    }
 
-	public void setPrice(String price) {
-		this.price = price;
-	}
+    public void setPrice(String price) {
+        this.price = price;
+    }
 
-	public String getArrow() {
-		return arrow;
-	}
+    public String getArrow() {
+        return arrow;
+    }
 
-	public void setArrow(String arrow) {
-		this.arrow = arrow;
-	}
+    public void setArrow(String arrow) {
+        this.arrow = arrow;
+    }
 
 }
